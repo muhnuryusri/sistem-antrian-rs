d275fc10eef211e7b3e82de7cee58655298a7c9f
muhnuryusri <muhnuryuusri@gmail.com>
2023-01-08 18:59:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sistem_informasi_antrian_rumah_sakit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sistem_informasi_antrian_rumah_sakit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
