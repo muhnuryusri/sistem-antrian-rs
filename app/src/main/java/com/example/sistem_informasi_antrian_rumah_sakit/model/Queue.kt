bfb1bebb64fbd711a1914ea7e19e860552cc6bd8
muhnuryusri <muhnuryuusri@gmail.com>
2023-01-08 22:35:19 +0800

    add models

---
+++ app/src/main/java/com/example/sistem_informasi_antrian_rumah_sakit/model/Queue.kt
@@ -0,0 +1,6 @@
+package com.example.sistem_informasi_antrian_rumah_sakit.model
+
+data class Queue (
+    var id: Int,
+    var antrian: Int,
+)
---
+++ app/src/main/java/com/example/sistem_informasi_antrian_rumah_sakit/model/User.kt
@@ -0,0 +1,8 @@
+package com.example.sistem_informasi_antrian_rumah_sakit.model
+
+data class User (
+    var id: Int,
+    var username: String,
+    var email: String,
+    var password: String
+)
