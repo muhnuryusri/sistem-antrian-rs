b9bdfd2744b6dcfc32f1e85336a97fa5b7a43d40
muhnuryusri <muhnuryuusri@gmail.com>
2023-01-12 23:32:07 +0800

    update login for admin

--- app/src/main/java/com/example/sistem_informasi_antrian_rumah_sakit/LoginActivity.kt
+++ app/src/main/java/com/example/sistem_informasi_antrian_rumah_sakit/LoginActivity.kt
@@ -26,23 +26,36 @@ class LoginActivity : AppCompatActivity() {
         binding.btnLogin.setOnClickListener {
             val email = binding.email.text.toString()
             val password = binding.password.text.toString()
-//            binding.progressBar.visibility = View.VISIBLE
 
             if (email.isEmpty() && password.isEmpty()) {
                 Toast.makeText(this@LoginActivity, "Masukkan username dan password anda", Toast.LENGTH_SHORT).show()
             } else {
                 auth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                            startActivity(intent)
-                            Toast.makeText(baseContext, "Login berhasil",
-                                Toast.LENGTH_SHORT).show()
-                            finish()
+                        if (auth.currentUser?.email == "admin@gmail.com") {
+                            if (it.isSuccessful) {
+                                val intent = Intent(this@LoginActivity, MainAdminActivity::class.java)
+                                startActivity(intent)
+                                Toast.makeText(baseContext, "Login admin berhasil",
+                                    Toast.LENGTH_SHORT).show()
+                                finish()
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(baseContext, "Username atau password anda salah",
+                                    Toast.LENGTH_SHORT).show()
+                            }
                         } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(baseContext, "Username atau password anda salah",
-                                Toast.LENGTH_SHORT).show()
+                            if (it.isSuccessful) {
+                                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                                startActivity(intent)
+                                Toast.makeText(baseContext, "Login berhasil",
+                                    Toast.LENGTH_SHORT).show()
+                                finish()
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(baseContext, "Username atau password anda salah",
+                                    Toast.LENGTH_SHORT).show()
+                            }
                         }
                     }
             }
@@ -51,17 +64,20 @@ class LoginActivity : AppCompatActivity() {
         binding.navLogin.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
-        }
-
-        binding.navLoginAdmin.setOnClickListener {
-            val intent = Intent(this, LoginAdminActivity::class.java)
-            startActivity(intent)
+            this.finish()
         }
     }
 
     private fun updateUI(currentUser: FirebaseUser?) {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        if (auth.currentUser?.email == "admin@gmail.com") {
+            val intent = Intent(this, MainAdminActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        } else {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        }
     }
 
 
