a39b7983cd9f27ca98f672bf8664c3af42ef60f6
muhnuryusri <muhnuryuusri@gmail.com>
2023-01-12 23:29:39 +0800

    update queue adapter

--- app/src/main/java/com/example/sistem_informasi_antrian_rumah_sakit/adapter/QueueAdapter.kt
+++ app/src/main/java/com/example/sistem_informasi_antrian_rumah_sakit/adapter/QueueAdapter.kt
@@ -1,18 +1,24 @@
 package com.example.sistem_informasi_antrian_rumah_sakit.adapter
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sistem_informasi_antrian_rumah_sakit.databinding.ItemQueueBinding
 import com.example.sistem_informasi_antrian_rumah_sakit.model.Queue
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-class QueueAdapter (private val queueList: ArrayList<Queue>) : RecyclerView.Adapter<QueueAdapter.ItemViewHolder>() {
+class QueueAdapter (val context: Context?, private val queueList: ArrayList<Queue>) : RecyclerView.Adapter<QueueAdapter.ItemViewHolder>() {
+    private lateinit var database: DatabaseReference
+
     fun setData(data: List<Queue>) {
         queueList.clear()
         queueList.addAll(data)
-        this.notifyItemChanged(0)
+        this.notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -23,10 +29,17 @@ class QueueAdapter (private val queueList: ArrayList<Queue>) : RecyclerView.Adap
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         if (position == 0) {
+            holder.bind(queueList[0])
             holder.binding.apply {
-                cardQueue.strokeColor = Color.GREEN
+                val queue = queueList[position]
+                cardQueue.setCardBackgroundColor(Color.GREEN)
                 tvAntrianGuide.text = "Antrian Dipanggil"
-                tvAntrianGuide.setTextColor(Color.GREEN)
+                tvNamaPasien.setTextColor(Color.WHITE)
+                tvNoAntrian.setTextColor(Color.WHITE)
+                tvAntrianGuide.setTextColor(Color.WHITE)
+                cardQueue.setOnClickListener {
+                    showDeleteDialog(queue)
+                }
             }
         } else {
             holder.bind(queueList[position])
@@ -42,5 +55,25 @@ class QueueAdapter (private val queueList: ArrayList<Queue>) : RecyclerView.Adap
                 tvNoAntrian.text = data.no.toString()
             }
         }
+    }
+
+    fun showDeleteDialog(queue: Queue) {
+        val builder = AlertDialog.Builder(context)
+
+        builder.setTitle("Hapus Antrian")
+        builder.setMessage("Anda ingin menghapus antrian ini?")
+
+        database = FirebaseDatabase.getInstance().reference.child("Queue")
+
+        builder.setPositiveButton("Ya") { _, _ ->
+            queue.id?.let { database.child(it).removeValue() }
+            setData(queueList)
+        }
+
+        builder.setNegativeButton("Tidak") { _, _ ->
+        }
+
+        val alert = builder.create()
+        alert.show()
     }
 }
--- app/src/main/java/com/example/sistem_informasi_antrian_rumah_sakit/model/Queue.kt
+++ app/src/main/java/com/example/sistem_informasi_antrian_rumah_sakit/model/Queue.kt
@@ -1,8 +1,10 @@
 package com.example.sistem_informasi_antrian_rumah_sakit.model
 
+import com.google.android.gms.tasks.Task
+
 data class Queue(
     var id: String? = null,
     var idUser: String? = null,
-    var no: Int? = 0,
+    var no: Int? = null,
     var name: String? = null,
 )
