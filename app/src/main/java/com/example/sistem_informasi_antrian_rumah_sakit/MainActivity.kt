2c0e94548324bbbc26d96e46e4ba42ad8d41c14d
muhnuryusri <muhnuryuusri@gmail.com>
2023-01-13 23:48:38 +0800

    fix last and first queue

--- app/src/main/java/com/example/sistem_informasi_antrian_rumah_sakit/MainActivity.kt
+++ app/src/main/java/com/example/sistem_informasi_antrian_rumah_sakit/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.sistem_informasi_antrian_rumah_sakit
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.sistem_informasi_antrian_rumah_sakit.databinding.ActivityMainBinding
 import com.example.sistem_informasi_antrian_rumah_sakit.model.Queue
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 
@@ -26,30 +29,81 @@ class MainActivity : AppCompatActivity() {
         databaseAuth =
             auth.currentUser?.let { FirebaseDatabase.getInstance().reference.child("Users").child(it.uid).child("name") }!!
 
-        var number = 1
+        val number = 1
 
         databaseQueue.addValueEventListener(object : ValueEventListener {
+            @SuppressLint("SetTextI18n")
             override fun onDataChange(snapshot: DataSnapshot) {
-                val antrianSekarang = databaseQueue.orderByKey().limitToFirst(1)
-                antrianSekarang.addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        for (data in snapshot.children) {
-                            val idUser = data.child("idUser").value
-                            val antrian = data.child("no").value
+                if (snapshot.exists()) {
+                    val antrianSekarang = databaseQueue.orderByKey().limitToFirst(1)
+                    antrianSekarang.addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            for (data in snapshot.children) {
+                                val idUser = data?.child("idUser")?.value
+                                val antrian = data?.child("no")?.getValue(Int::class.java)
 
-                            binding.tvNoAntrianSekarang.text = antrian.toString()
-                            if (idUser == auth.currentUser!!.uid) {
-                                binding.tvNoAntrianDipanggil.visibility = View.VISIBLE
-                            } else {
-                                binding.tvNoAntrianDipanggil.visibility = View.GONE
+                                binding.tvNoAntrianSekarang.text = antrian.toString()
+
+                                if (idUser.toString() == auth.currentUser?.uid.toString()) {
+                                    binding.tvNoAntrian.text = antrian.toString()
+                                    binding.tvNoAntrianDipanggil.visibility = View.VISIBLE
+                                    binding.btnDaftarAntrian.isEnabled = false
+                                } else {
+                                    binding.tvNoAntrianDipanggil.visibility = View.GONE
+                                }
                             }
                         }
-                    }
 
-                    override fun onCancelled(error: DatabaseError) {
-                    }
+                        override fun onCancelled(error: DatabaseError) {
+                        }
 
-                })
+                    })
+
+                    val antrianTerbaru = databaseQueue.orderByKey().limitToLast(1)
+                    antrianTerbaru.addValueEventListener(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            for (data in snapshot.children) {
+                                val antrian = data?.child("no")?.getValue(Int::class.java)
+                                binding.btnDaftarAntrian.setOnClickListener {
+                                    databaseAuth.addValueEventListener(object : ValueEventListener {
+                                        override fun onDataChange(snapshot: DataSnapshot) {
+                                            val queue = snapshot.value.toString()
+                                            if (antrian != null) {
+                                                addQueue(antrian+1, queue)
+                                            }
+                                        }
+
+                                        override fun onCancelled(error: DatabaseError) {
+                                        }
+                                    })
+                                }
+                            }
+                        }
+
+                        override fun onCancelled(error: DatabaseError) {
+                        }
+
+                    })
+                } else {
+                    binding.apply {
+                        tvNoAntrianSekarang.apply {
+                            text = "Belum ada antrian"
+                            textSize = 20F
+                            setTextColor(Color.RED)
+                        }
+                        btnDaftarAntrian.setOnClickListener {
+                            databaseAuth.addValueEventListener(object : ValueEventListener {
+                                override fun onDataChange(snapshot: DataSnapshot) {
+                                    val queue = snapshot.value.toString()
+                                    addQueue(number, queue)
+                                }
+
+                                override fun onCancelled(error: DatabaseError) {
+                                }
+                            })
+                        }
+                    }
+                }
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -57,22 +111,11 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-        binding.btnDaftarAntrian.setOnClickListener {
-            databaseAuth.addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    val queue = snapshot.value.toString()
-                    addQueue(number++, queue)
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                }
-            })
-        }
-
         binding.btnLogout.setOnClickListener {
             auth.signOut()
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+            this.finish()
             Toast.makeText(this,"Logout berhasil", Toast.LENGTH_SHORT).show()
         }
     }
@@ -86,7 +129,7 @@ class MainActivity : AppCompatActivity() {
         val queue = id?.let { auth.currentUser?.let { it1 -> Queue(it, it1.uid, noAntrian, namaAntrian) } }
 
         binding.tvNoAntrian.text = noAntrian.toString()
-//        binding.btnDaftarAntrian.isEnabled = false
+        binding.btnDaftarAntrian.isEnabled = false
 
         if (id != null) {
             databaseQueue.child(id).setValue(queue).addOnSuccessListener {
